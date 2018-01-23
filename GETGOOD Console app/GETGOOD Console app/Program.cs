ce3a0011626a668f910ceb98171e5588ffb119fa
Luke Waltenbaugh <lew81@BATCH>
2018-01-23 18:35:45 -0500

    adding var

---
+++ GETGOOD Console app/GETGOOD Console app/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GETGOOD_Console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testing a comment
+
+            int var = 0;
+        }
+    }
+}
